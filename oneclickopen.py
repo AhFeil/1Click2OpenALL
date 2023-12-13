7c54eecb2c18112a104898dd620050bedfbe43f2
ahfei <vgamebox@outlook.com>
2023-12-13 18:44:19 +0800

    support md format - extract url from md content

--- oneclickopen.py
+++ oneclickopen.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request, session
 from urllib.parse import urlparse
+import re
+
+
+def extract_urls(text):
+    pattern = r'\[.*?\]\((.*?)\)'  # 正则表达式模式，匹配 [任意字符](网址)
+    urls = re.findall(pattern, text)  # 在文本中查找所有匹配的网址
+    return urls
+
 
 app = Flask(__name__)
 
@@ -15,15 +23,23 @@ def index():
 
 @app.route('/open_websites', methods=['POST'])
 def open_websites():
-    websites = request.form.get('websites')  # 获取表单中的网址
-    website_list = websites.split('\n')  # 将多行网址拆分为列表
-    website_list = [url.strip() for url in website_list if url.strip()]  # 去除空行
+    website_content = request.form.get('websites')  # 获取表单中的文本
+    website_content_list = website_content.split('\n')  # 将多行文本拆分为列表
+    website_content_list = [url.strip() for url in website_content_list if url.strip()]  # 去除空行
+    website_list = []   # 存放真正的网址
     
-    # 处理网址，确保它们是完整的URL
-    for i in range(len(website_list)):
-        parsed_url = urlparse(website_list[i])
-        if not parsed_url.scheme:
-            website_list[i] = "http://" + website_list[i]
+    # 处理网址，
+    for i in range(len(website_content_list)):
+        # 如果是 md 格式，则可以提取出网址链接
+        url_list = extract_urls(website_content_list[i])
+        if url_list:
+            website_list.extend(url_list)
+        else:
+            # 若不匹配 md，则认为是纯网址，确保它们是完整的URL
+            parsed_url = urlparse(website_content_list[i])
+            if not parsed_url.scheme:
+                website_content_list[i] = "http://" + website_content_list[i]
+            website_list.append(website_content_list[i])
 
     # 更新会话中的网址列表
     session['websites'] = website_list
